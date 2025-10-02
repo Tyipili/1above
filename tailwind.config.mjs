fb3f72c4a61b0d3f706e9c187a9cd09471b7ddd0
Tyipili <91148233+Tyipili@users.noreply.github.com>
2025-10-02 14:57:06 -0400

    Update tailwind.config.mjs

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,19 +1,15 @@
-/** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./src/app/**/*.{js,jsx}",
-    "./src/components/**/*.{js,jsx}"
-  ],
+  content: ["./src/app/**/*.{js,jsx}", "./src/components/**/*.{js,jsx}"],
   theme: {
-  extend: {
-    colors: {
-      brand: {
-        green: "#1C352D",
-        gold: "#C8A951",
-        offwhite: "#FAFAFA",
+    extend: {
+      colors: {
+        brand: {
+          green: "#1C352D",
+          gold: "#C8A951",
+          offwhite: "#FAFAFA",
+        }
       }
     }
-  }
-},
+  },
   plugins: [],
-};
+}
